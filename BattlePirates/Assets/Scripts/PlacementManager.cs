ecea25c083741c7b07817f27249d448125eb14ec
KishanFowler <vogelaarkishan@gmail.com>
2025-03-28 11:37:34 +0100

    drag and drop systeem geimplementeerd en een shipbase aangemaakt

--- BattlePirates/Assets/Scripts/PlacementManager.cs
+++ BattlePirates/Assets/Scripts/PlacementManager.cs
@@ -2,33 +2,52 @@ using UnityEngine;
 
 public class PlacementManager : MonoBehaviour
 {
-    [SerializeField] private InputManager InputManager;
-    [SerializeField] public GridManager GridManager;
+    private GridManager _gridManager;
+    private GameManager _gameManager;
+    private GameStates _gameState;
 
-    void OnEnable()
+    private void Start()
     {
-        InputManager.Click += Click;
-
-        InputManager.OpenMenu += OpenMenu;
+        _gridManager = GameObject.Find("GridManager").GetComponent<GridManager>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    void OnDisable()
+    private void OnMouseDown()
     {
-        InputManager.Click -= Click;
-
-        InputManager.OpenMenu -= OpenMenu;
+        //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, (Mathf.Sign(-transform.rotation.z) * (-transform.rotation.z + 90)), transform.rotation.w);
     }
 
-    void Click()
+    private void OnMouseDrag()
     {
-        if (GridManager.GetTileAtPosition(InputManager.GetSelectedPosition()).CanBeHit)
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9));
+    }
+
+    private void OnMouseUp()
+    {
+        _gameState = _gameManager.gameState;
+
+        RaycastHit hit;
+        //layermask 3 is a custom layermask for hittable tiles
+        LayerMask layerMask = 3;
+        Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.forward), out hit, Mathf.Infinity, layerMask);
+
+        Debug.Log(hit.point);
+
+        Tile tile = _gridManager.GetTileAtPosition(hit.point);
+
+        Debug.Log(tile.transform.position);
+
+        if (tile != null)
         {
+            if (_gameState == GameStates.PlayerTurn && _gameManager.CanPlayerAttack && tile.CanBeHit)
+            {
 
+            }
+
+            if (_gameState == GameStates.PreparationPhase && tile.CanBePlaced)
+            {
+                transform.position = tile.transform.position;
+            }
         }
-    }
-
-    void OpenMenu()
-    {
-
     }
 }
