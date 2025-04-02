3d787566af4e7ce176a3f353ac8314f047f8c70e
BrendanOssebaar <90763311+BrendanOssebaar@users.noreply.github.com>
2025-04-02 14:35:32 +0200

    All is ready for Demo

--- BattlePirates/Assets/Scripts/ButtonHandler.cs
+++ BattlePirates/Assets/Scripts/ButtonHandler.cs
@@ -11,25 +11,31 @@ public class ButtonHandler : MonoBehaviour
     public UIDocument uiDocument;
     private Dictionary<string, System.Action> m_ButtonActions;
     private VisualElement m_Help;
+    private VisualElement m_HelpScreen;
     private VisualElement m_SettingsPanel;
     private Button m_SettingsButton;
     void Start()
     {
-        VisualElement root = uiDocument.rootVisualElement;
+        var root = uiDocument.rootVisualElement;
         m_ButtonActions = new Dictionary<string, Action>
         {
             { "Play", Play },
             { "Settings", ToggleSettings },
             { "Quit", QuitGame },
-            { "Help", Help},
-            { "Forfeit", Forfeit}
+            { "HelpButton", Help},
+            { "Forfeit", Forfeit},
+            { "Again", Again},
+            { "MainMenu", MainMenu}
         };
         m_SettingsPanel = root.Q<VisualElement>("SettingsPanel");
-        m_Help = root.Q<Button>("HelpButton");
-        m_SettingsButton = root.Q<Button>("SettingsButton");
+        m_HelpScreen = root.Q<VisualElement>("HelpScreen");
         foreach (var kvp in m_ButtonActions)
         {
             Button button = root.Q<Button>(kvp.Key);
+            if (m_HelpScreen != null)
+            {
+                m_HelpScreen.style.display = DisplayStyle.None;
+            }
             if (button != null)
             {
                 string buttonName = kvp.Key;
@@ -42,15 +48,31 @@ public class ButtonHandler : MonoBehaviour
         }
     }
 
+    private void MainMenu()
+    {
+        Debug.Log("Main Menu Button clicked, returning to main menu");
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    private void Again()
+    {
+        Debug.Log("Again button clicked, returning to planning phase");
+        SceneManager.LoadScene("PlanningPhase2");
+    }
+
     private void Forfeit()
     {
         Debug.Log("Forfeit button clicked. Checking for confirmation");
+        SceneManager.LoadScene("DefeatScreen");
     }
 
     private void Help()
     {
         Debug.Log("Help Button clicked. Opening Help Box");
-        m_Help.visible = true;
+        if (m_HelpScreen.style.display == DisplayStyle.None)
+            m_HelpScreen.style.display = DisplayStyle.Flex; // Of wat je gebruikt (bijv. Grid)
+        else
+            m_HelpScreen.style.display = DisplayStyle.None;
     }
 
     private void OnButtonClicked(string buttonName)
