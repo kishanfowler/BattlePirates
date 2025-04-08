4cf001b0a397f172b8c1e5efc3191a1a692b4e3b
BrendanOssebaar <90763311+BrendanOssebaar@users.noreply.github.com>
2025-04-08 15:45:35 +0200

    Start placement AI ships

--- BattlePirates/Assets/Scripts/AIManager.cs
+++ BattlePirates/Assets/Scripts/AIManager.cs
@@ -10,19 +10,78 @@ public class AIManager : MonoBehaviour
     public GameObject GameManager;
     private int _shotsAvailable;
     private bool _canShoot;
-    private List<Vector2> _shootableTargets = new List<Vector2>();
-    private ShipBase[] _enemyShips;
+    private List<Vector2> _shootableTargets = new();
     private Tile _tile;
     private Tile _targetTile;
     private GridManager _gridManager;
     private GameManager _gameManager;
+    [SerializeField] private ShipBase[] ShipPrefabs;
+    private List<ShipBase> _aiShipsToPlace = new ();
+    private ShipManager _shipManager;
 
-    // public AIManager()
-    // {
-    //     _enemyShips = new[] {
-    //         gameObject.AddComponent<ShipBase>()
-    //     };
-    // }
+    private void InitializeAIShips()
+    {
+        _shipManager = gameObject.GetComponent<ShipManager>();
+        ShipPrefabs = _shipManager._ships;
+        foreach (var shipPrefab in ShipPrefabs)
+        {
+            ShipBase aiShip = Instantiate(shipPrefab);
+            aiShip.gameObject.SetActive(false);
+            _aiShipsToPlace.Add(aiShip);
+        }
+    }
+    private bool CanPlaceShipVertically(Vector2 StartPos, int Length)
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            Vector2 pos = new Vector2(StartPos.x, StartPos.y + i);
+
+            if (_gridManager.GetAllTilePositions().Contains(pos)) return false; // buiten de grid
+            if (_gridManager.GetTileAtPosition(pos).IsOccupied) return false; // overlapping
+        }
+
+        return true;
+    }
+
+    private void Awake()
+    {
+        InitializeAIShips();
+    }
+
+    private void AIPlaceShips()
+    {
+        GridManager grid = GridPositions.GetComponent<GridManager>();
+        foreach (var ship in _aiShipsToPlace)
+        {
+            bool placed = false;
+            while (!placed)
+            {
+                int x = Random.Range(0, grid.width);
+                int y = Random.Range(0, grid.height - ship.ShipLength + 1);
+
+                Vector2 startPos = new Vector2(x, y);
+
+                if (CanPlaceShipVertically(startPos, ship.ShipLength))
+                {
+                    PlaceShipVertically(startPos, ship.ShipLength);
+                    placed = true;
+                }
+
+
+
+            }
+        }
+    }
+
+    private void PlaceShipVertically(Vector2 StartPos, int ShipLength)
+    {
+        for (int i = 0; i < ShipLength; i++)
+        {
+            Vector2 pos = new Vector2(StartPos.x, StartPos.y + i);
+            Tile tile = _gridManager.GetTileAtPosition(pos);
+            tile.IsOccupied = true;
+        }
+    }
 
     private void InitShots()
     {
