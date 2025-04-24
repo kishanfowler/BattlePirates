e9da5917b44582625161366f9aba56e6faf963dd
KishanFowler <vogelaarkishan@gmail.com>
2025-04-24 13:12:42 +0200

    powerup begin gemaakt

--- BattlePirates/Assets/Scripts/AttackManager.cs
+++ BattlePirates/Assets/Scripts/AttackManager.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,12 +7,31 @@ public class AttackManager : MonoBehaviour
 {
     private GameManager _gameManager;
     private GridManager _gridManager;
+    private ShipManager _shipManager;
     private GameStates _gameState;
+    public GameObject MistPrefab;
+    public GameObject PlusIndicator;
+    public GameObject DutchManPrefab;
+    private bool _canAISpecialAttack = true;
+    private bool _canPlayerSpecialAttack = true;
+    private bool _plusAttack = true;
+
+
+    public enum SpecialAttacks
+    {
+        Mist,
+        Coin,
+        Plus,
+        Dutchman
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         _gridManager = GameObject.Find("GridManager").GetComponent<GridManager>();
+        _shipManager = GameObject.Find("GridManager").GetComponent<ShipManager>();
+
     }
 
     private void FixedUpdate()
@@ -31,6 +52,106 @@ public class AttackManager : MonoBehaviour
             tile.OnHit();
             _gameManager.GameState = GameStates.AITurn;
             _gameManager.CanPlayerAttack = false;
+        }
+    }
+
+    void SpecialAttack(SpecialAttacks attackType)
+    {
+        if (_gameManager.GameState == GameStates.PlayerTurn && _canPlayerSpecialAttack)
+        {
+            switch (attackType)
+            {
+                case SpecialAttacks.Mist:
+                    Instantiate(MistPrefab, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1), Quaternion.identity);
+                    _canPlayerSpecialAttack = false;
+                    break;
+                case SpecialAttacks.Coin:
+                    //animatie stuffs
+                    if (UnityEngine.Random.Range(0, 1) == 0)
+                    {
+                        int randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Length);
+                        while (!_shipManager._ships[randomShip].IsPlayerShip)
+                        {
+                            randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Length);
+                            break;
+                        }
+                        if (_shipManager._ships[randomShip].IsPlayerShip)
+                        {
+                            Tile tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
+                            while (tile.CanBeHit == false)
+                            {
+                                tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
+                                break;
+                            }
+                            tile.OnHit();
+                        }
+                    }
+                    else
+                    {
+                        int randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Length);
+                        while (_shipManager._ships[randomShip].IsPlayerShip)
+                        {
+                            randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Length);
+                            break;
+                        }
+                        if (!_shipManager._ships[randomShip].IsPlayerShip)
+                        {
+                            Tile tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
+                            while (tile.CanBeHit == false)
+                            {
+                                tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
+                                break;
+                            }
+                            tile.OnHit();
+                        }
+                    }
+                    _canPlayerSpecialAttack = false;
+                    break;
+                case SpecialAttacks.Plus:
+                    var indicator = Instantiate(PlusIndicator, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1), Quaternion.identity);
+                    while (_plusAttack)
+                    {
+                        indicator.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9));
+                        if (Input.GetKeyDown(KeyCode.Mouse0))
+                        {
+                            Tile tile = _gridManager.GetTileAtPosition(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9)));
+                            if (tile)
+                            {
+                                tile.OnHit();
+                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x + 1, tile.transform.position.y)))
+                                {
+                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x + 1, tile.transform.position.y)).OnHit();
+                                }
+                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x - 1, tile.transform.position.y)))
+                                {
+                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x - 1, tile.transform.position.y)).OnHit();
+                                }
+                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y +1)))
+                                {
+                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y + 1)).OnHit();
+                                }
+                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y - 1)))
+                                {
+                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y - 1)).OnHit();
+                                }
+                                _plusAttack = false;
+                                _canPlayerSpecialAttack = false;
+                                break;
+                            }
+                        }
+                    }
+                    _canPlayerSpecialAttack = false;
+                    break;
+                case SpecialAttacks.Dutchman:
+                    var ship = Instantiate(DutchManPrefab, new Vector3(-8, 6, -1), quaternion.identity);
+                    ship.GetComponent<ShipBase>().IsPlayerShip = true;
+                    _canPlayerSpecialAttack = false;
+                    break;
+            }
+        }
+        if(_gameManager.GameState == GameStates.AITurn && _canAISpecialAttack)
+        {
+
         }
     }
 }
--- BattlePirates/Assets/Scripts/ShipBase.cs
+++ BattlePirates/Assets/Scripts/ShipBase.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class ShipBase : MonoBehaviour
 {
     public int ShipLength;
-
+    public bool IsPlayerShip;
     [SerializeField] private Sprite SunkenShipSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        IsPlayerShip = true;
     }
 
     // Update is called once per frame
