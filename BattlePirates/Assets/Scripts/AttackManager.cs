6c28a78099159011ed62de0611414a1a62115e2b
KishanFowler <vogelaarkishan@gmail.com>
2025-05-16 14:19:31 +0200

    coin flip powerup gemaakt

--- BattlePirates/Assets/Scripts/AttackManager.cs
+++ BattlePirates/Assets/Scripts/AttackManager.cs
@@ -1,6 +1,5 @@
-using System;
+using Spine.Unity;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackManager : MonoBehaviour
@@ -8,14 +7,18 @@ public class AttackManager : MonoBehaviour
     private GameManager _gameManager;
     private GridManager _gridManager;
     private ShipManager _shipManager;
+    private AIManager _aiManager;
     private GameStates _gameState;
-    public GameObject MistPrefab;
-    public GameObject PlusIndicator;
-    public GameObject DutchManPrefab;
     private bool _canAISpecialAttack = true;
     private bool _canPlayerSpecialAttack = true;
     private bool _plusAttack = true;
-
+    public GameObject MistPrefab;
+    public GameObject PlusIndicator;
+    public GameObject DutchManPrefab;
+    public SkeletonAnimation _SkeletonAnimation;
+    public AnimationReferenceAsset HeadsAnimation;
+    public AnimationReferenceAsset TailsAnimation;
+    public GameObject CoinAnimationObject;
 
     public enum SpecialAttacks
     {
@@ -30,8 +33,8 @@ public class AttackManager : MonoBehaviour
     {
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         _gridManager = GameObject.Find("GridManager").GetComponent<GridManager>();
-        _shipManager = GameObject.Find("GridManager").GetComponent<ShipManager>();
-
+        _shipManager = GameObject.Find("ShipManager").GetComponent<ShipManager>();
+        _aiManager = GameObject.Find("AIManager").GetComponent<AIManager>();
     }
 
     private void FixedUpdate()
@@ -40,13 +43,15 @@ public class AttackManager : MonoBehaviour
         {
             Attack();
         } 
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            DoSpecialAttack(SpecialAttacks.Coin);
+        }
     }
 
     void Attack()
     {
-        // //position * 1.05 because of the orthographic projection, otherwise the last row and line of the grid get skipped.
-        // Tile tile = _gridManager.GetTileAtPosition(Camera.main.ScreenToWorldPoint(new Vector3((Input.mousePosition.x + Input.mousePosition.x) /1.95f, (Input.mousePosition.y+Input.mousePosition.y)/1.9f, -1)));
-        // _gameState = _gameManager.GameState;
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10f; // afstand tot camera bij orthografisch
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
@@ -65,7 +70,7 @@ public class AttackManager : MonoBehaviour
         }
     }
 
-    void SpecialAttack(SpecialAttacks attackType)
+    public void DoSpecialAttack(SpecialAttacks attackType)
     {
         if (_gameManager.GameState == GameStates.PlayerTurn && _canPlayerSpecialAttack)
         {
@@ -76,14 +81,20 @@ public class AttackManager : MonoBehaviour
                     _canPlayerSpecialAttack = false;
                     break;
                 case SpecialAttacks.Coin:
-                    //animatie stuffs
-                    if (UnityEngine.Random.Range(0, 1) == 0)
+                    var RandomNumber = UnityEngine.Random.Range(0, 1);
+                    // If the coin is Tails, hit a player ship
+                    if (RandomNumber == 0)
                     {
+                        CoinAnimationObject.SetActive(true);
+                        _SkeletonAnimation.state.SetAnimation(0, TailsAnimation, false);
                         int randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Count);
                         while (!_shipManager._ships[randomShip].IsPlayerShip)
                         {
                             randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Count);
-                            break;
+                            if (_shipManager._ships[randomShip].IsPlayerShip)
+                            {
+                                break;
+                            }
                         }
                         if (_shipManager._ships[randomShip].IsPlayerShip)
                         {
@@ -91,25 +102,34 @@ public class AttackManager : MonoBehaviour
                             while (tile.CanBeHit == false)
                             {
                                 tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
-                                break;
+                                if(tile.CanBeHit)
+                                {
+                                    break;
+                                }
                             }
                             tile.OnHit();
                         }
                     }
+                    // If the coin is Heads, hit an enemy ship
                     else
                     {
-                        int randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Count);
-                        while (_shipManager._ships[randomShip].IsPlayerShip)
+                        CoinAnimationObject.SetActive(true);
+                        _SkeletonAnimation.state.SetAnimation(0, HeadsAnimation, false);
+                        int randomShip = UnityEngine.Random.Range(0, _aiManager.AIShipsToPlace.Count);
+                        while (_aiManager.AIShipsToPlace[randomShip].IsPlayerShip)
                         {
-                            randomShip = UnityEngine.Random.Range(0, _shipManager._ships.Count);
-                            break;
+                            randomShip = UnityEngine.Random.Range(0, _aiManager.AIShipsToPlace.Count);
+                            if (!_aiManager.AIShipsToPlace[randomShip].IsPlayerShip)
+                            {
+                                break;
+                            }
                         }
-                        if (!_shipManager._ships[randomShip].IsPlayerShip)
+                        if (!_aiManager.AIShipsToPlace[randomShip].IsPlayerShip)
                         {
-                            Tile tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
+                            Tile tile = _aiManager.AIShipsToPlace[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _aiManager.AIShipsToPlace[randomShip].ShipLength)].GetTile();
                             while (tile.CanBeHit == false)
                             {
-                                tile = _shipManager._ships[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _shipManager._ships[randomShip].ShipLength)].GetTile();
+                                tile = _aiManager.AIShipsToPlace[randomShip].GetComponentsInChildren<ShipPlacer>()[UnityEngine.Random.Range(0, _aiManager.AIShipsToPlace[randomShip].ShipLength)].GetTile();
                                 break;
                             }
                             tile.OnHit();
@@ -118,32 +138,47 @@ public class AttackManager : MonoBehaviour
                     _canPlayerSpecialAttack = false;
                     break;
                 case SpecialAttacks.Plus:
-                    var indicator = Instantiate(PlusIndicator, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1), Quaternion.identity);
+                    var screenPosition = Input.mousePosition;
+                    screenPosition.z = 9f; // z-distance from camera
+                    Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+                    // Instantiate the indicator at the world position
+                    var indicator = Instantiate(PlusIndicator, worldPosition, Quaternion.identity);
+
                     while (_plusAttack)
                     {
-                        indicator.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9));
+                        // Update indicator position
+                        screenPosition = Input.mousePosition;
+                        screenPosition.z = 9f;
+                        worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+                        indicator.transform.position = worldPosition;
+
                         if (Input.GetKeyDown(KeyCode.Mouse0))
                         {
-                            Tile tile = _gridManager.GetTileAtPosition(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9)));
-                            if (tile)
+                            Tile centerTile = _gridManager.GetTileAtPosition(worldPosition);
+                            if (centerTile != null)
                             {
-                                tile.OnHit();
-                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x + 1, tile.transform.position.y)))
+                                centerTile.OnHit();
+
+                                // Define 4 cardinal directions
+                                Vector2[] directions = new Vector2[]
                                 {
-                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x + 1, tile.transform.position.y)).OnHit();
-                                }
-                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x - 1, tile.transform.position.y)))
+                                    Vector2.right,
+                                    Vector2.left,
+                                    Vector2.up,
+                                    Vector2.down
+                                };
+
+                                foreach (Vector2 dir in directions)
                                 {
-                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x - 1, tile.transform.position.y)).OnHit();
+                                    Vector2 neighborPos = (Vector2)centerTile.transform.position + dir;
+                                    Tile neighborTile = _gridManager.GetTileAtPosition(neighborPos);
+                                    if (neighborTile != null)
+                                    {
+                                        neighborTile.OnHit();
+                                    }
                                 }
-                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y +1)))
-                                {
-                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y + 1)).OnHit();
-                                }
-                                if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y - 1)))
-                                {
-                                    _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y - 1)).OnHit();
-                                }
+
                                 _plusAttack = false;
                                 _canPlayerSpecialAttack = false;
                                 break;
@@ -152,6 +187,40 @@ public class AttackManager : MonoBehaviour
                     }
                     _canPlayerSpecialAttack = false;
                     break;
+                /*var indicator = Instantiate(PlusIndicator, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1), Quaternion.identity);
+                while (_plusAttack)
+                {
+                    indicator.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9));
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        Tile tile = _gridManager.GetTileAtPosition(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 9)));
+                        if (tile)
+                        {
+                            tile.OnHit();
+                            if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x + 1, tile.transform.position.y)))
+                            {
+                                _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x + 1, tile.transform.position.y)).OnHit();
+                            }
+                            if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x - 1, tile.transform.position.y)))
+                            {
+                                _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x - 1, tile.transform.position.y)).OnHit();
+                            }
+                            if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y + 1)))
+                            {
+                                _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y + 1)).OnHit();
+                            }
+                            if (_gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y - 1)))
+                            {
+                                _gridManager.GetTileAtPosition(new Vector2(tile.transform.position.x, tile.transform.position.y - 1)).OnHit();
+                            }
+                            _plusAttack = false;
+                            _canPlayerSpecialAttack = false;
+                            break;
+                        }
+                    }
+                }
+                _canPlayerSpecialAttack = false;
+                break;*/
                 case SpecialAttacks.Dutchman:
                     var ship = Instantiate(DutchManPrefab, new Vector3(-8, 6, -1), quaternion.identity);
                     ship.GetComponent<ShipBase>().IsPlayerShip = true;
